8228d1d8abfe5c0b5884e7ab19a82a903a3f3128
Galdea <gabriel.aldea@gmail.com>
2022-12-14 18:52:47 +0000

    falta la visualizacion por celular

--- src/js/component/cardgrid.js
+++ src/js/component/cardgrid.js
@@ -2,22 +2,45 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+
+const cardData = [
+  {
+    imgSrc: "https://picsum.photos/id/237/200/300",
+    title: "Card 1",
+    text: "Primera Carta con contenido"
+  },
+  {
+    imgSrc: "https://picsum.photos/id/238/200/300",
+    title: "Card 2",
+    text: "Segunda Carta con contenido"
+  },
+  {
+    imgSrc: "https://picsum.photos/id/239/200/300",
+    title: "Card 3",
+    text: "Tercera Carta con contenido"
+  },
+  {
+    imgSrc: "https://picsum.photos/id/240/200/300",
+    title: "Card 4",
+    text: "Cuarta Carta con contenido"
+  }
+];
 
 function CardGrid() {
   return (
     <Row xs={2} md={4} className="g-4">
-      {Array.from({ length: 4 }).map((_, idx) => (
-        <Col>
+      {cardData.map((card, idx) => (
+        <Col key={idx}>
           <Card>
-            <Card.Img variant="top" src="https://picsum.photos/id/237/200/300" />
+            <Card.Img variant="top" src={card.imgSrc} />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
+              <Card.Title>{card.title}</Card.Title>
+              <Card.Text>{card.text}</Card.Text>
             </Card.Body>
+            <Card.Footer>
+              <Button>Find Out More!</Button>
+            </Card.Footer>
           </Card>
         </Col>
       ))}
@@ -26,29 +49,3 @@ function CardGrid() {
 }
 
 export default CardGrid;
-
-// function CardGrid() {
-//   return (
-//     <Card style={{ width: '18rem' }}>
-//       <Card.Img variant="top" src={imgUrl} />
-//       <Card.Body>
-//         <Card.Title>{title}</Card.Title>
-//         <Card.Text>
-//           {description}
-//         </Card.Text>
-//         <Button variant="primary" href={buttonUrl}>{buttonLabel}</Button>
-//       </Card.Body>
-//     </Card>
-//   );
-// }
-// <
-// 	title="Bob Dylan", 
-// 	imageUrl="dgg", 
-// 	description="dsg", 
-// 	buttonUrl="ffdg", 
-// 	buttonLabel="Find out More!" 
-// 	/>
-
-// export default CardGrid;
-  
-  
